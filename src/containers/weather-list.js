fec41b940f78d49ab43d8ad367e93b4a6a0c6295
Tomás Andrés Flores Alvear <tomfloresa@Tomass-MBP.home>
2017-05-11 02:06:12 -0300

    first commit

---
+++ src/containers/weather-list.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import Chart from '../components/chart';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+
+    return(
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
+      </tr>
+    );
+  }
+
+  render() {
+    return(
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>City</th>
+              <th>Temperature</th>
+              <th>Pressure</th>
+              <th>Humidity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.weather.map(this.renderWeather)}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({weather}) {
+  return {weather};
+}
+
+export default connect(mapStateToProps)(WeatherList);
